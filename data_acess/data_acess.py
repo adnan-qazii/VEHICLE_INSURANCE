b7debc0b93723fcb373fd9924a3835448360a6ca
qazi_adnan <qazi.adnan.2k1@gmail.com>
2025-08-13 21:31:49 +0530

    Add DataAccess class for MongoDB data retrieval and update requirements

---
+++ data_acess/data_acess.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from configration.mongo_db_connection import MongoDBConnection
+from logger import logging
+from exception import MyException
+
+
+
+
+class DataAccess:
+
+    def __init__(self):
+        load_dotenv()
+        self.mongo_db_connection = MongoDBConnection()
+
+    def fetch_data(self, collection_name, query=None):
+        try:
+            if query is None:
+                query = {}
+            documents = self.mongo_db_connection.db[collection_name].find(query)
+            return pd.DataFrame(documents)
+        except Exception as e:
+            logging.error(f"Error fetching data: {e}")
+            raise MyException("Error fetching data", sys) from e
